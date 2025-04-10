1b33ab5e54d4c8766d6be212cc35099b55403b13
Olafey <olamidearemu2000@yahoo.com>
2025-04-10 08:55:55 +0100

    creation of a signup jsx

--- src/(pages)/HomePage.jsx
+++ src/(pages)/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import SignUp from "./signUp/SignUp";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <SignUp />
     </div>
   );
 };
